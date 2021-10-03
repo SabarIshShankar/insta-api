6f62a8fb3a24452562bd5e766886c5c997f12b6e
sabarIshs19 <66781581+sabarIshs19@users.noreply.github.com>
2021-10-03 11:31:04 +0530

    update

---
+++ src/routes/user.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const {
+    getUsers, getUser, follow, unfollow, feed, searchUser, editUser
+} = require("../controllers/user");
+
+router.route("/").get(protect, getUsers);
+router.route("/").put(protect, editUser);
+router.route("/feed").get(protect, feed);
+router.route("/search").get(searchUser);
+router.route("/:username").get(protect, getUser);
+router.route("/:id/follow").get(protect, follow);
+router.route("/:id/unfollow").get(protect, unfollow);
+
+module.exports = router;
