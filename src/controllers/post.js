44925e2421d302a24939b7e60353443f81bf03fe
sabarIshs19 <66781581+sabarIshs19@users.noreply.github.com>
2021-10-03 18:37:20 +0530

    Update post.js

--- src/controllers/post.js
+++ src/controllers/post.js
@@ -156,3 +156,91 @@ exports.addComment = asyncHandler(async(req, res, next) => {
         .execPopulate();
     res.status(200).json({success: true, data: comment});
 });
+
+exports.deleteComment = asyncHandler(async(req, res, next) => {
+    const post = await Post.findById(req.params.id);
+
+    if(!post){
+        return next({
+            message: `No post found for id ${req.params.id}`,
+            statusCode: 404,
+        });
+    }
+    const comment = await Comment.findOne({
+        _id: req.params.commentId,
+        post: req.params.id
+    });
+
+    if(!comment){
+        return next({
+            message: `No comment found for id ${req.params.id}`,
+            statusCode: 404
+        });
+    }
+    if(comment.user.toString() !== req.user.id){
+        return next({
+            message: "Not authorized to delete this comment",
+            statusCode: 401
+        });
+    }
+
+    const index = post.comments.indexOf(comments._id);
+    post.comments.splice(index, 1);
+    post.commentsCount = post.commentsCount - 1;
+    await post.save();
+
+    await comment.remove();
+
+    res.status(200).json({success: true, data: {}});
+});
+
+exports.searchPost = asyncHandler(async(req, res, next) => {
+    if(!req.query.caption && !req.query.tag){
+        return next({
+            message: "Please enter caption or tag",
+            statuCode: 400,
+        });
+    }
+
+    let posts = [];
+
+    if(req.query.caption){
+        const regex = new RegExp(req.query.caption, "i");
+        posts = await Post.fing({caption: regex})
+    }
+    if(req.query.tag){
+        posts = posts.concat([await Post.find({tags: req.query.tag})]);
+    }
+
+    res.status(200).json({success: true, data: posts});
+});
+
+exports.toggleSave = asyncHandler(async(req, res, next) => {
+    const post = await Post.findById(req.params.id);
+
+    if(!post){
+        return next({
+            message: `No post found for id ${req.params.id}`,
+            statusCode: 404,
+        });
+    }
+
+    const {user}=req;
+
+    if(user.savedPosts.includes(req.params.id)){
+        console.log("removng saved post");
+        await user.findByIdAndUpdate(user.id, {
+            $pull: {savedPosts: req.params.id},
+        });
+    } else {
+        console.log("saving post");
+        await User.findByIdAndUpdate(user.id, {
+            $push: {
+                savedPosts: req.params.id
+            },
+        });
+    }
+    res.status(200).json({
+        success: true, data: {}
+    })
+})
